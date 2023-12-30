687db6de4cff04e63fd95c8af94911a98c091ff2
leonm <leon.maderic@gmail.com>
2023-12-30 11:29:56 +0100

    Recipe default constructor

--- app/src/main/java/hr/ferit/leonmaderic/recipeapp/data/Recipe.kt
+++ app/src/main/java/hr/ferit/leonmaderic/recipeapp/data/Recipe.kt
@@ -18,22 +18,22 @@ import hr.ferit.leonmaderic.recipeapp.Routes
 import hr.ferit.leonmaderic.recipeapp.ui.*
 
 data class Recipe(
-    var id: String,
-    val image: String,
-    val title: String,
-    val category: String,
-    val cookingTime: String,
-    val energy: String,
-    val rating: String,
-    val description: String,
-    val reviews: String,
-    val ingredients: List<Ingredient>,
-    var isFavorited: Boolean
+    var id: String = "",
+    val image: String = "",
+    val title: String = "",
+    val category: String = "",
+    val cookingTime: String = "",
+    val energy: String = "",
+    val rating: String = "",
+    val description: String = "",
+    val reviews: String = "",
+    val ingredients: List<Ingredient> = listOf(),
+    var isFavorited: Boolean = false
 )
 data class Ingredient(
-    val image: String,
-    val title: String,
-    val subtitle: String
+    val image: String = "",
+    val title: String = "",
+    val subtitle: String = ""
 )
 
 
